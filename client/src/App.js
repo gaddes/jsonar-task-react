d051b9287be1ddef5b34b11f5d43bc772dc77af1
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-04-16 07:57:28 -0700

    Re-write App.js to make use of modern React syntax.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,45 @@
+// import React, { Component } from 'react';
+// import Header from './Header';
+// import Login from './Login';
+// import './_reset.scss';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <Login />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
-import Header from './Header';
-import Login from './Login';
-import './_reset.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: []
+    };
+  }
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(members => this.setState({ members: members }));
+  }
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Login />
-      </React.Fragment>
+      <div className="Users">
+        <h1>Users</h1>
+        {this.state.members.map(member =>
+          <div key={member.id}>{member.name} {member.surname} - {member.email}</div>
+        )}
+      </div>
     );
   }
-}
+};
 
 export default App;
